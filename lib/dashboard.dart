453623b98b6fb432da1aa26e15c37f87ae8094ff
João Nascimento <joao_nascimento6@hotmail.com>
2023-12-12 11:21:58 +0000

    dashboard

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,99 +1,102 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
-class DashboardPage extends StatefulWidget {
-  const DashboardPage({Key? key, required this.title}) : super(key: key);
-
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key, required this.title});
   final String title;
-
-  @override
-  State<DashboardPage> createState() => _DashboardPageState();
-}
-
-class _DashboardPageState extends State<DashboardPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Olisipo Manager'),
-        ),
-        body: Container(
-          margin: const EdgeInsets.symmetric(vertical: 20),
-          height: 300,
-          child: ListView(
-            // This next line does the trick.
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Stack(
-                children: [
-                  Container(
-                    width: 300,
-                    height: 211,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      image: DecorationImage(
-                        image: AssetImage('images/imagem1.jpg'),
-                        fit: BoxFit.fitWidth,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    bottom: 0,
-                    left: 16,
-                    child: SizedBox(
-                      width: 148.93,
-                      height: 16.45,
-                      child: Opacity(
-                        opacity: 0.90,
-                        child: Text(
-                          'Noticia nº1',
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 16,
-                            fontFamily: 'Inter',
-                            fontWeight: FontWeight.w600,
-                            height: 0,
-                          ),
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 20),
+        height: 300,
+        child: ListView(
+          // This next line does the trick.
+          scrollDirection: Axis.horizontal,
+          children: <Widget>[
+            Stack(
+              children: [
+                Positioned(
+                  bottom: 90,
+                  left: 16,
+                  child: Container(
+                    width: 150,
+                    height: 18,
+                    child: Opacity(
+                      opacity: 0.90,
+                      child: Text(
+                        'Noticia nº1',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 228, 186,
+                              186), // Set text color to contrast with the image
+                          fontSize: 16,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w600,
+                          height: 0,
                         ),
                       ),
                     ),
                   ),
-                ],
-              ),
-              Container(
-                width: 300,
-                height: 211,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage('images/imagem1.jpg'),
-                    fit: BoxFit.fitWidth,
+                ),
+                Container(
+                  width: 300,
+                  height: 211,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    image: DecorationImage(
+                      image: AssetImage('images/imagem1.jpg'),
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                width: 300,
-                height: 211,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage('images/imagem1.jpg'),
-                    fit: BoxFit.fitWidth,
-                  ),
+              ],
+            ),
+            Container(
+              width: 300,
+              height: 211,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('images/imagem1.jpg'),
+                  fit: BoxFit.fitWidth,
                 ),
               ),
-              Container(
-                width: 300,
-                height: 211,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  image: DecorationImage(
-                    image: AssetImage('images/imagem1.jpg'),
-                    fit: BoxFit.fitWidth,
-                  ),
+            ),
+            Container(
+              width: 300,
+              height: 211,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('images/imagem1.jpg'),
+                  fit: BoxFit.fitWidth,
                 ),
               ),
-            ],
-          ),
-        ));
+            ),
+            Container(
+              width: 300,
+              height: 211,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage('images/imagem1.jpg'),
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+            ),
+            Container(
+              width: 300,
+              height: 250,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10), color: Colors.black),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/registo/registo.dart
+++ lib/registo/registo.dart
@@ -1,157 +1,154 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 
-class RegistoPage extends StatefulWidget {
-  const RegistoPage({Key? key, required this.title}) : super(key: key);
-
+class RegistoPage extends StatelessWidget {
+  const RegistoPage({super.key, required this.title});
   final String title;
-
-  @override
-  State<RegistoPage> createState() => _RegistoPageState();
-}
-
-class _RegistoPageState extends State<RegistoPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Olisipo Manager'),
       ),
-      body: Column(
-        children: [
-          Text(
-            'Registo',
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 30,
-              fontFamily: 'Inter',
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          Container(
-            width: 343,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 230, 230, 230),
-              borderRadius: BorderRadius.circular(17),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 10), // Move 10 pixels to the right
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Nome',
-                  labelStyle: TextStyle(
-                    color: Color.fromARGB(255, 125, 125, 125),
-                    fontSize: 16,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                  ),
-                  border: InputBorder.none,
-                ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Registo',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 30,
+                fontFamily: 'Inter',
+                fontWeight: FontWeight.w600,
               ),
             ),
-          ),
-          SizedBox(height: 30),
-          // Email TextField with rounded border
-          Container(
-            width: 343,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 230, 230, 230),
-              borderRadius: BorderRadius.circular(17),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 10), // Move 10 pixels to the right
-              child: TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(
-                    color: Color.fromARGB(255, 125, 125, 125),
-                    fontSize: 16,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w500,
-                  ),
-                  border: InputBorder.none,
-                ),
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 230, 230, 230),
+                borderRadius: BorderRadius.circular(17),
               ),
-            ),
-          ),
-          SizedBox(height: 20),
-          // Password TextField with rounded border
-          Container(
-            width: 343,
-            height: 50,
-            decoration: BoxDecoration(
-              color: Color.fromARGB(255, 230, 230, 230),
-              borderRadius: BorderRadius.circular(17),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 10), // Move 10 pixels to the right
-              child: Stack(
-                children: [
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      labelText: 'Password',
-                      labelStyle: TextStyle(
-                        color: Color.fromARGB(255, 125, 125, 125),
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w500,
-                      ),
-                      border: InputBorder.none,
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 10), // Move 10 pixels to the right
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Nome',
+                    labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 125, 125, 125),
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
                     ),
+                    border: InputBorder.none,
                   ),
-                  Positioned(
-                    right: 10, // Align to the right
-                    top: 16,
-                    child: Text(
-                      'Mostrar',
-                      style: TextStyle(
-                        color: Color(0xFF32D700),
-                        fontSize: 16,
-                        fontFamily: 'Inter',
-                        fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            // Email TextField with rounded border
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 230, 230, 230),
+                borderRadius: BorderRadius.circular(17),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 10), // Move 10 pixels to the right
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(
+                      color: Color.fromARGB(255, 125, 125, 125),
+                      fontSize: 16,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w500,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            // Password TextField with rounded border
+            Container(
+              width: 343,
+              height: 50,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 230, 230, 230),
+                borderRadius: BorderRadius.circular(17),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(
+                    left: 10), // Move 10 pixels to the right
+                child: Stack(
+                  children: [
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 125, 125, 125),
+                          fontSize: 16,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w500,
+                        ),
+                        border: InputBorder.none,
                       ),
+                    ),
+                    Positioned(
+                      right: 10, // Align to the right
+                      top: 16,
+                      child: Text(
+                        'Mostrar',
+                        style: TextStyle(
+                          color: Color(0xFF32D700),
+                          fontSize: 16,
+                          fontFamily: 'Inter',
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 100),
+            Container(
+              height: 51,
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 100, vertical: 12),
+              decoration: ShapeDecoration(
+                color: Color(0xFF32D700),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Registar',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w600,
                     ),
                   ),
                 ],
               ),
             ),
-          ),
-          SizedBox(height: 100),
-          Container(
-            height: 51,
-            padding: const EdgeInsets.symmetric(horizontal: 100, vertical: 12),
-            decoration: ShapeDecoration(
-              color: Color(0xFF32D700),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  'Registar',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 18,
-                    fontFamily: 'Inter',
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
